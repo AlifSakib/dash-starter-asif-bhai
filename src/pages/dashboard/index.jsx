8adfee73868f4d5dcf1abeaa93a3975b659e9b5a
alifsakib <alifsakib@gmail.com>
2024-07-03 17:39:37 +0600

    init

---
+++ src/pages/dashboard/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Card from "@/components/ui/Card";
+import CompanyTable from "../../components/partials/Table/company-table";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Card title="Starter Kit">Your Dashboard</Card>
+    </div>
+  );
+};
+
+export default Dashboard;
